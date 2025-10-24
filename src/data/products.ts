51dca0edc5c14bd831e1ee29819cea879bdea1e2
Natanael Melo <natanael.melo47@gmail.com>
2025-10-24 19:33:04 -0300

    add: adicionando novos sabores e novo item no cardapio

--- src/data/products.ts
+++ src/data/products.ts
@@ -4,54 +4,64 @@ export const PRODUCTS: Product[] = [
   {
     id: "1",
     name: "Geladinho de Ninho com Nutella",
-    price: 3.99,
+    price: 4.99,
     imageUrl:
       "https://images.unsplash.com/photo-1761240135088-1bd4efa45848?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735",
-    category: "TRADICIONAIS",
+    category: "PREMIUM",
     description:
       "Delicioso geladinho cremoso de leite ninho com nutella, combinação perfeita de chocolate e avelã.",
-    tags: ["Novo"],
+    tags: ["Popular"],
   },
   {
     id: "2",
-    name: "Geladinho Sensação",
+    name: "Geladinho Morango com Chocolate",
     price: 3.99,
     imageUrl:
       "https://images.unsplash.com/photo-1761240135088-1bd4efa45848?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735",
     category: "TRADICIONAIS",
     description:
-      "Geladinho sensação com camadas de chocolate e baunilha, uma explosão de sabores clássicos.",
+      "Geladinho delicioso de morango com chocolate, combinação perfeita de frutas vermelhas e chocolate cremoso.",
     tags: ["Promo"],
   },
   {
     id: "3",
-    name: "Geladinho de Coco",
+    name: "Geladinho de Coco com Ninho",
     price: 3.99,
     imageUrl:
       "https://images.unsplash.com/photo-1761240135088-1bd4efa45848?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735",
     category: "TRADICIONAIS",
     description:
-      "Geladinho refrescante de coco, feito com leite de coco natural e raspas crocantes.",
+      "Geladinho refrescante de coco, feito com leite de coco natural e raspas crocantes e Ninho.",
     tags: ["Popular"],
   },
   {
     id: "4",
-    name: "Geladinho de Maraucuja",
+    name: "Geladinho de Maracujá",
     price: 3.99,
     imageUrl:
       "https://images.unsplash.com/photo-1761240135088-1bd4efa45848?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735",
-    category: "PREMIUM",
+    category: "TRADICIONAIS",
     description:
       "Geladinho tropical de maracujá, com polpa fresca e um toque cítrico irresistível.",
   },
   {
     id: "5",
-    name: "Geladinho de Doce de Leite com Paçoquita",
-    price: 3.99,
+    name: "Geladinho de Maracujá com Chocolate",
+    price: 4.49,
     imageUrl:
       "https://images.unsplash.com/photo-1761240135088-1bd4efa45848?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735",
     category: "PREMIUM",
     description:
-      "Geladinho cremoso de doce de leite com paçoquita, combinação perfeita e indulgente.",
+      "Geladinho cremoso de maracujá com chocolate, uma combinação tropical e indulgente.",
+  },
+  {
+    id: "6",
+    name: "Geladinho de Ninho com Morango",
+    price: 4.99,
+    imageUrl:
+      "https://images.unsplash.com/photo-1761240135088-1bd4efa45848?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735",
+    category: "PREMIUM",
+    description:
+      "Geladinho cremoso de Ninho com Morango, combinação perfeita de Ninho, leite condensado e Morango.",
   },
 ];
