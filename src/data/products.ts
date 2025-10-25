374cbdbe70126a7f331648a75703ccc56d5e809d
Natanael Melo <natanael.melo47@gmail.com>
2025-10-25 14:50:00 -0300

    add: adicionando link para as fotos de outros sabores

--- src/data/products.ts
+++ src/data/products.ts
@@ -6,7 +6,7 @@ export const PRODUCTS: Product[] = [
     name: "Geladinho de Ninho com Nutella",
     price: 4.99,
     imageUrl:
-      "https://images.unsplash.com/photo-1761240135088-1bd4efa45848?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735",
+      "https://images.unsplash.com/photo-1761414375278-c3371ec0cabc?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735",
     category: "PREMIUM",
     description:
       "Delicioso geladinho cremoso de leite ninho com nutella, combinação perfeita de chocolate e avelã.",
@@ -28,7 +28,7 @@ export const PRODUCTS: Product[] = [
     name: "Geladinho de Coco com Ninho",
     price: 3.99,
     imageUrl:
-      "https://images.unsplash.com/photo-1761240135088-1bd4efa45848?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735",
+      "https://images.unsplash.com/photo-1761414375428-eae632779821?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=627",
     category: "TRADICIONAIS",
     description:
       "Geladinho refrescante de coco, feito com leite de coco natural e raspas crocantes e Ninho.",
@@ -43,8 +43,8 @@ export const PRODUCTS: Product[] = [
     category: "TRADICIONAIS",
     description:
       "Geladinho tropical de maracujá, com polpa fresca e um toque cítrico irresistível.",
-   tags: ["Popular"],
-    },
+    tags: ["Popular"],
+  },
   {
     id: "5",
     name: "Geladinho de Maracujá com Chocolate",
@@ -55,7 +55,7 @@ export const PRODUCTS: Product[] = [
     description:
       "Geladinho cremoso de maracujá com chocolate, uma combinação tropical e indulgente.",
     tags: ["Promo"],
-    },
+  },
   {
     id: "6",
     name: "Geladinho de Morango com Ninho",
@@ -65,6 +65,17 @@ export const PRODUCTS: Product[] = [
     category: "PREMIUM",
     description:
       "Geladinho cremoso de Ninho com Morango, combinação perfeita de Ninho, leite condensado e Morango.",
-  tags: ["Novo"],
-    },
+    tags: ["Novo"],
+  },
+  {
+    id: "7",
+    name: "Geladinho de Coco com Chocolate",
+    price: 4.99,
+    imageUrl:
+      "https://images.unsplash.com/photo-1761414375428-eae632779821?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=627",
+    category: "PREMIUM",
+    description:
+      "Geladinho cremoso de Coco com Chocolate, combinação perfeita de Coco, leite condensado e Chocolate.",
+    tags: ["Novo"],
+  },
 ];
